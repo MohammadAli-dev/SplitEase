6ef17c208f131d9463104fccbd5f61558e49090d
Mohammad Ali <mohammad8.ali6@gmail.com>
2026-01-12 14:26:20 +0530

    style(GoogleSignInButton): Remove unused imports
    Removed unused imports (Spacer, Icon, painterResource, etc.) that were
    intended for a future icon implementation but are currently unused.
    Clean imports = clean code.

--- app/src/main/java/com/splitease/ui/auth/GoogleSignInButton.kt
+++ app/src/main/java/com/splitease/ui/auth/GoogleSignInButton.kt
@@ -1,19 +1,14 @@
 package com.splitease.ui.auth
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.HorizontalDivider
